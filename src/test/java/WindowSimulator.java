6f1ce7d4185e60393c8bfba8ed06429348f223e4
Kyle Chen <chenchhaykyle@gmail.com>
2023-03-08 01:34:57 -0800

    edited for cmdline prompts

--- src/test/java/WindowSimulator.java
+++ src/test/java/WindowSimulator.java
@@ -2,38 +2,38 @@ import java.util.Random;
 public class WindowSimulator {
 
     public static void main(String[] args){
-//        System.out.println("Starting WindowSimulator...");
-//        //Check for invalid inputs
-//        if(args.length!=6){
-//            System.out.println("Invalid inputs. Insufficient amount of arguments");
-//            return;
-//        }
-//        int sws;
-//        int rws;
-//        int channel_length;
-//        float prob_not_recv;
-//        float prob_not_ackd;
-//        int num_frames;
-//        try{
-//            sws = Integer.parseInt(args[0]);
-//            rws = Integer.parseInt(args[1]);
-//            channel_length = Integer.parseInt(args[2]);
-//            prob_not_recv = Float.parseFloat(args[3]);
-//            prob_not_ackd = Float.parseFloat((args[4]));
-//            num_frames = Integer.parseInt(args[5]);
-//
-//            if((sws >127 || sws<1) || (rws>127 || rws<1)){
-//                System.out.println("Invalid inputs. Invalid sws/rws arguments");
-//                return;
-//            }else if(prob_not_recv <0 || prob_not_recv>1 || prob_not_ackd < 0 || prob_not_ackd>1){
-//                System.out.println("Invalid inputs. Invalid prob_not_recv/prob_not_ackd");
-//                return;
-//            }
-//        } catch(NumberFormatException e){
-//            e.printStackTrace();
-//            System.out.println("Invalid inputs");
-//            return;
-//        }
+       System.out.println("Starting WindowSimulator...");
+       //Check for invalid inputs
+       if(args.length!=6){
+           System.out.println("Invalid inputs. Insufficient amount of arguments");
+           return;
+       }
+       int sws;
+       int rws;
+       int channel_length;
+       float prob_not_recv;
+       float prob_not_ackd;
+       int num_frames;
+       try{
+           sws = Integer.parseInt(args[0]);
+           rws = Integer.parseInt(args[1]);
+           channel_length = Integer.parseInt(args[2]);
+           prob_not_recv = Float.parseFloat(args[3]);
+           prob_not_ackd = Float.parseFloat((args[4]));
+           num_frames = Integer.parseInt(args[5]);
+
+           if((sws >127 || sws<1) || (rws>127 || rws<1)){
+               System.out.println("Invalid inputs. Invalid sws/rws arguments");
+               return;
+           }else if(prob_not_recv <0 || prob_not_recv>1 || prob_not_ackd < 0 || prob_not_ackd>1){
+               System.out.println("Invalid inputs. Invalid prob_not_recv/prob_not_ackd");
+               return;
+           }
+       } catch(NumberFormatException e){
+           e.printStackTrace();
+           System.out.println("Invalid inputs");
+           return;
+       }
 
 
 //        byte frame= 4;
@@ -49,17 +49,18 @@ public class WindowSimulator {
 //        byte test3 = (byte)test2;
 //        System.out.println(test3);
 
-        int num_frames = 4;
-        Station s1 = new Station(2,2,0);
-        Station r1 = new Station(2,2,0);
-        Pipe senderPipe = new Pipe(2);
-        Pipe receiverPipe = new Pipe(2);
+        Station s1 = new Station(sws,rws,0);
+        Station r1 = new Station(sws,rws,0);
+        Pipe senderPipe = new Pipe(channel_length);
+        Pipe receiverPipe = new Pipe(channel_length);
         int steps = 0;
         int counter = 0;
         boolean notDone = true;
         int sumUtilizations = 0;
         float averageUtilization;
+        // frame sent by receiver held for comparison
         byte[] receiverFrame;
+        // byte values for the last ack frame
         int[] lastAck = {num_frames, 255, 255, 255, 254};
         int match = 0;
 
